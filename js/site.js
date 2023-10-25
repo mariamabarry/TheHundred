bd08b2391a850e52aa19f3c064eb6f71ec038301
mariamabarry <barry.hadja16@gmail.com>
2023-10-24 23:31:20 -0400

    Completed 95% of project
    - Changed logo
    - Built the app and code pages

--- js/site.js
+++ js/site.js
@@ -1,3 +1,57 @@
-function HelloWorld(){
-    alert("hello world!");
+
+/*CONTROLLER FUNCTION */
+//get the values from the Page
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //validate input
+    if(Number.isInteger(startValue) && Number.isInteger(endValue))
+    {
+        //call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+        //call displayNumbers
+        displayNumbers(numbers); 
+    }
+    else
+        {
+            alert("You must enter integers.");
+        }
+}
+
+/*LOGIC FUNCTIONS*/
+//generate numbers from startValue to endValue
+function generateNumbers(start, end){
+    let values = [];
+    range = end - start;
+    for(let i=start; i <= end; i++){
+        values.push(i);
+    }
+    return values;
+}
+
+/* DISPLAY OR VIEW FUNCTIONS*/
+//display the numbers with the even numbers being bold
+function displayNumbers(nums){
+    let templateRows = "";
+    for(let i = 0; i < nums.length; i++)
+    {
+        let className = "";
+        let num = nums[i];
+        if (num % 2 == 0)
+        {
+            className = "even";
+        }
+        else
+        {
+            className = "odd";
+        }
+
+        templateRows += `<tr><td class="${className}">${num}</td></tr>`;
+
+    }
+    document.getElementById("results").innerHTML = templateRows;
 }
